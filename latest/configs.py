343184b4dadbac4ffb1f88e5ee1690f3cd9cd0f4
Rumen Dangovski <Darumen@dhcp-18-111-96-234.dyn.mit.edu>
2017-09-28 22:59:56 -0400

    added new nlp and drum III

---
+++ latest/configs.py
@@ -0,0 +1,73 @@
+
+def get_config(model):
+    if model == 'enwik8':
+        return enwik_config()
+    if model == 'ptb':
+        return ptb_config()
+    if model == 'drum': 
+        return drum_config() 
+    else:
+        raise ValueError("Invalid model: %s", model)
+
+class enwik_config(object):
+    """Enwik8 config."""
+    init_scale = 0.01
+    learning_rate = 0.001
+    max_grad_norm = 1.0
+    num_layers = 2
+    num_steps = 100
+    cell_size = 1200
+    hyper_size = 1500
+    embed_size = 256
+    max_epoch = 35
+    max_max_epoch = max_epoch
+    keep_prob = 0.75
+    zoneout_h = 0.95
+    zoneout_c = 0.7
+    lr_decay = 0.1
+    batch_size = 128
+    vocab_size = 205
+    fast_layers = 4
+    dataset = 'enwik8'
+
+
+class ptb_config(object):
+    """PTB config."""
+    init_scale = 0.01
+    learning_rate = 0.002
+    max_grad_norm = 1.0
+    num_layers = 2
+    num_steps = 150
+    cell_size = 700
+    hyper_size = 400
+    embed_size = 128
+    max_epoch = 200
+    max_max_epoch = max_epoch
+    keep_prob = 0.65
+    zoneout_h = 0.9
+    zoneout_c = 0.5
+    lr_decay = 0.1
+    batch_size = 128
+    vocab_size = 50
+    fast_layers = 2
+    dataset = 'ptb'
+
+class drum_config(object):
+    """PTB config."""
+    init_scale = 0.01
+    learning_rate = 0.002
+    max_grad_norm = 5.0
+    num_steps = 50
+    cell_size = 2000
+    embed_size = 128
+    max_epoch = 200
+    max_max_epoch = max_epoch
+    keep_prob = 0.65
+    zoneout_h = 0.9
+    lr_decay = 0.1
+    batch_size = 64
+    vocab_size = 50
+    T_norm = 0.5
+    use_layer_norm = True
+    use_zoneout = True 
+    dataset = 'ptb'
